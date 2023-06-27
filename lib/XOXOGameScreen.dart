4e9014fd4329fa0a63803fca3eed5c0dce5ea52d
Kenneth Yousuf <kenneth.yousuf156@gmail.com>
2023-06-26 18:30:01 -0700

    1

---
+++ lib/XOXOGameScreen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class XOXOGameScreen extends StatefulWidget {
+  const XOXOGameScreen({super.key});
+
+  @override
+  State<XOXOGameScreen> createState() => _XOXOGameScreenState();
+}
+
+class _XOXOGameScreenState extends State<XOXOGameScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:xoxo/XOXOGameScreen.dart';
+
+void main() {
+  runApp(XOXOGame());
+}
+
+class XOXOGame extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'XOXO Game',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: XOXOGameScreen(),
+    );
+  }
+}
