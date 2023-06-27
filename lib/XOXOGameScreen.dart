4219839ce8ac3d4998b900e2a0196542067b26c2
Kenneth Yousuf <kenneth.yousuf156@gmail.com>
2023-06-26 19:16:06 -0700

    6

--- lib/XOXOGameScreen.dart
+++ lib/XOXOGameScreen.dart
@@ -15,17 +15,19 @@ class _XOXOGameScreenState extends State<XOXOGameScreen> {
   late String winner;
   late bool gameEnded;
   @override
-   void initState() {
+  void initState() {
     super.initState();
     initializeBoard();
   }
-   void initializeBoard() {
+
+  void initializeBoard() {
     board = List.generate(3, (_) => List.filled(3, ''));
     isPlayer1Turn = true;
     winner = '';
     gameEnded = false;
   }
-   void makeMove(int row, int col) {
+
+  void makeMove(int row, int col) {
     if (gameEnded || board[row][col] != '') return;
 
     setState(() {
@@ -34,7 +36,7 @@ class _XOXOGameScreenState extends State<XOXOGameScreen> {
       checkWinner();
     });
   }
-  
+
   void checkWinner() {
     // Check rows
     for (int row = 0; row < 3; row++) {
@@ -56,7 +58,7 @@ class _XOXOGameScreenState extends State<XOXOGameScreen> {
         return;
       }
     }
-     // Check diagonals
+    // Check diagonals
     if (board[0][0] == board[1][1] &&
         board[0][0] == board[2][2] &&
         board[0][0] != '') {
@@ -64,14 +66,14 @@ class _XOXOGameScreenState extends State<XOXOGameScreen> {
       gameEnded = true;
       return;
     }
-     if (board[0][2] == board[1][1] &&
+    if (board[0][2] == board[1][1] &&
         board[0][2] == board[2][0] &&
         board[0][2] != '') {
       winner = board[0][2];
       gameEnded = true;
       return;
     }
-    
+
     // Check for a draw
     bool isBoardFull = true;
     for (int row = 0; row < 3; row++) {
@@ -88,69 +90,75 @@ class _XOXOGameScreenState extends State<XOXOGameScreen> {
       winner = 'Draw';
     }
   }
-    void resetGame() {
+
+  void resetGame() {
     setState(() {
       initializeBoard();
     });
   }
-   
+
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromARGB(255, 68, 174, 255),
       appBar: AppBar(
         centerTitle: true,
         title: Text("XOXO Game"),
       ),
-
       body: Column(
-    children: [
-      Expanded(
-        flex: 1,
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Text(
-            gameEnded ? 'Winner: $winner' : 'Next Turn: ${isPlayer1Turn ? "Player 1 (X)" : "Player 2 (O)"}',
-            style: TextStyle(fontSize: 20),
-          ),
-        ),
-      ),
-      Expanded(
-        flex: 5,
-        child: GridView.builder(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 3,
-          ),
-          itemCount: 9,
-          itemBuilder: (context, index) {
-            final row = index ~/ 3;
-            final col = index % 3;
-            return GestureDetector(
-              onTap: () {
-                makeMove(row, col);
-              },
-              child: Container(
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.black,
-                  ),
-                ),
-                child: Center(
-                  child: Text(
-                    board[row][col],
-                    style: TextStyle(fontSize: 40),
-                  ),
-                ),
+        children: [
+          Expanded(
+            flex: 1,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                gameEnded
+                    ? 'Winner: $winner'
+                    : 'Next Turn: ${isPlayer1Turn ? "Player 1 (X)" : "Player 2 (O)"}',
+                style: TextStyle(fontSize: 20,
+                fontWeight: FontWeight.bold),
               ),
-            );
-          },
-        ),
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+              ),
+              itemCount: 9,
+              itemBuilder: (context, index) {
+                final row = index ~/ 3;
+                final col = index % 3;
+                return GestureDetector(
+                  onTap: () {
+                    makeMove(row, col);
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.black,
+                      ),
+                    ),
+                    child: Center(
+                      child: Text(
+                        board[row][col],
+                        style: TextStyle(fontSize: 40),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(18.0),
+            child: ElevatedButton(
+              onPressed: resetGame,
+              child: Text('Reset'),
+            ),
+          ),
+        ],
       ),
-      
-      ElevatedButton(
-        onPressed: resetGame,
-        child: Text('Reset'),
-      ),
-    ],
-  ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class XOXOGame extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: XOXOGameScreen(),
+      debugShowCheckedModeBanner:false,
     );
   }
 }
